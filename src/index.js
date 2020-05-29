1d5f3193a87d9b034debfc37bff12baeabf7aca3
shuaijie <shuaijie@amazon.com>
2020-05-28 23:52:22 -0700

    add some shimming example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+  // Assume we are in the context of `window`
+  this.alert('Hmmm, this probably isn\'t a great idea...')
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -11,10 +12,21 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: require.resolve('./src/index.js'),
+        use: 'imports-loader?this=>window',
+      },
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: 'Output Management',
+    }),
+    new webpack.ProvidePlugin({
+      join: ['lodash', 'join'],
     }),
   ],
   output: {
